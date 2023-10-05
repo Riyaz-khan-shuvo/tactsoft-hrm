833dac127dbb0c9705f9a932e2237cd8453a5997
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-10-05 16:15:03 +0600

    admin layout and frontend layout design has done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AdminLayout from './admin/SideNavbar/adminLayout'
+import { useRouter } from 'next/router';
+import FrontendLayout from '@/Component/FrontendLayout';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isAdminRoute = router.pathname.startsWith('/admin');
+  const Layout = isAdminRoute ? AdminLayout : FrontendLayout;
   return (
-    <AdminLayout>
+    <Layout>
       <Component {...pageProps} />
-    </AdminLayout>
+    </Layout>
   )
 }
